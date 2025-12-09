7aac69787643996d742439ef728ea2049fe28fe9
Osvadeb <osvadeb.1101@gmail.com>
2025-12-08 19:16:16 -0600

    login-roles-usuarioAdmin

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->role === 'admin';
     }
+
+    public function esRecepcionista()
+    {
+        return $this->role === 'recepcionista';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// 4 rutas nuevas de prueba para los tipos de usuario
+Route::get('/admin', function () {
+    Gate::authorize('solo-admin');
+    return "Bienvenido, Administrador";
+})->middleware(['auth']);
+
+Route::get('/recepcion', function () {
+    Gate::authorize('solo-recepcionista');
+    return "Bienvenido, Recepcionista";
+})->middleware(['auth']);
+
+Route::get('/panel-general', function () {
+    Gate::authorize('admin-o-recepcionista');
+    return "Acceso permitido";
+})->middleware(['auth']);
+
+//probar el admin
+Route::get('/admin-panel', function () {
+    Gate::authorize('solo-admin');
+    return "Bienvenido Administrador";
+})->middleware('auth');
+
+require __DIR__.'/auth.php';
