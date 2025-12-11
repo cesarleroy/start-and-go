d0db598f0c3320b4fc513a2f9f2a25ccb43d3725
Osvadeb <osvadeb.1101@gmail.com>
2025-12-11 01:46:54 -0600

    Ultimos ajustes al modulo de alumnos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -148,6 +148,6 @@
 
 <!-- Bootstrap JS Bundle (con Popper) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
+@stack('scripts')
 </body>
 </html>
